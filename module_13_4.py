0dafb58e2baeace77683e35a69bce96f253d251c
Андрей Шеремет <seheremet.and@yandex.ru>
2024-11-06 14:41:32 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,67 @@
+# Машина состояний
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer('Для подсчёта калорий необходимы некоторые данные. ' 
+                         'Пожалуйста, пишите все значения без букв и аббревиатур.')
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой рост:')
+    await UserState.growth.set()
+
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    data = await state.get_data()
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    try:
+        weight = float(data['weight'])
+        growth = float(data['growth'])
+        age = int(data['age'])
+        calories = 10 * weight + 6.25 * growth - 5 * age - 161
+        await message.answer(f"Ваша норма калорий - {calories}")
+    except ValueError:
+        await message.answer("Ошибка: все значения должны быть числовыми.")
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
